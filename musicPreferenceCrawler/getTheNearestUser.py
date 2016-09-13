9c81a6e151661546606bf80f25e97b6f289f49a0
xuguang.lv <xuguanglv@localhost>
2016-09-13 13:33:09 +0800

    选出距离最近的两个元素

---
+++ musicPreferenceCrawler/getTheNearestUser.py
@@ -0,0 +1,25 @@
+import pymongo
+
+client = pymongo.MongoClient("localhost", 27017)
+db = client.musicDistance
+cursor = db.musicDistance.find()
+
+i = 0
+globalNearestDistance = 1.0
+nearestPair = ['','']
+for item in cursor:
+    userId = item['userId']
+    distanceList = item['distanceList']
+    nearestDistance = 1.0
+    nearestUser = ''
+    for ele in distanceList:
+        if ele['distance'] < nearestDistance:
+            nearestDistance = ele['distance']
+            nearestUser = ele['userId']
+    print str(i) + ' The nearest neighbor of user ' + userId + ' is ' + nearestUser + ', with the distance of ' + str(nearestDistance)
+    if nearestDistance < globalNearestDistance:
+        globalNearestDistance = nearestDistance
+        nearestPair[0] = userId
+        nearestPair[1] = nearestUser
+    i += 1
+print nearestPair[0] + ' ' + nearestPair[1] + ' ' + str(globalNearestDistance)
